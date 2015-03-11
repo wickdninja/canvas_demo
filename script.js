28c84c7c79d2078af87447d6e6a901d61426f64b
Nate <nross63@gmail.com>
2015-03-11 16:38:56 -0400

    upate

--- script.js
+++ script.js
@@ -14,7 +14,7 @@
 
             canvas.width = img1.width;
             canvas.height = img1.height;
-            canvas.getContext('2d').drawImage(img1, 0, 0, img1.width, img1.height);
+            canvas.getContext('2d').drawImage(data, 0, 0, img1.width, img1.height);
             var dataUrl = canvas.toDataURL();
 
             img2.src = dataUrl;
@@ -23,8 +23,6 @@
 
         fileInput.onchange = function() {
             var file = document.querySelector('input[type=file]').files[0];
-
-
             if (file) {
                 reader.readAsDataURL(file);
             } else {
