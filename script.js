d57097c2edd2e5ea57a4886a7bdf71b8f9e9323d
Nate <nross63@gmail.com>
2015-03-11 17:50:46 -0400

    update

--- script.js
+++ script.js
@@ -22,13 +22,14 @@
 
                 var width = img.width / 4;
                 var height = img.height / 4;
+
                 canvas.width = width;
                 canvas.height = height;
                 window.setTimeout(function() {
                     ctx.drawImage(img, 0, 0, width, height);
                     var dataUrl = canvas.toDataURL('image/jpeg');
                     img2.src = dataUrl;
-                }, 1000);
+                }, 3000);
 
             }, 100);
 
