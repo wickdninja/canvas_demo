bbf26b3598b24921353a8875d72dcf74d07d7d75
Nate <nross63@gmail.com>
2015-03-11 17:47:33 -0400

    update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
     window.onload = function() {
         var maxSize = 900;
-        
+
         var img1 = document.getElementById('img1');
         var img2 = document.getElementById('img2');
         var canvas = document.getElementById('canvas');
@@ -20,23 +20,12 @@
                 img.src = data;
                 console.log('img: ' + img.width + ' x ' + img.height);
 
-                var width = img1.width;
-                var height = img1.height;
-
-                if (width > height) {
-                    if (width > maxSize) {
-                        height *= maxSize / width;
-                        width = maxSize;
-                    }
-                } else {
-                    if (height > maxSize) {
-                        width *= maxSize / width;
-                        height = maxSize;
-                    }
-                }
-                
+                var width = img.width / 4;
+                var height = img.height / 4;
+                canvas.width = width;
+                canvas.height = height;
                 window.setTimeout(function() {
-                    ctx.drawImage(img1, 0, 0, img1.width, img1.height);
+                    ctx.drawImage(img, 0, 0, width, height);
                     var dataUrl = canvas.toDataURL('image/jpeg');
                     img2.src = dataUrl;
                 }, 1000);
