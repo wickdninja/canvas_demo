975047d2df289594b5ea038acc2a1942fe075a30
Nate <nross63@gmail.com>
2015-03-11 16:43:53 -0400

    upate

--- script.js
+++ script.js
@@ -14,7 +14,7 @@
 
             canvas.width = img1.width;
             canvas.height = img1.height;
-            canvas.getContext('2d').drawImage(data, 0, 0, img1.width, img1.height);
+            canvas.getContext('2d').drawImage(img1, 0, 0, img1.width, img1.height);
             var dataUrl = canvas.toDataURL();
 
             img2.src = dataUrl;
